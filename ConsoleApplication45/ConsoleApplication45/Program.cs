853f2178d55f5ba0540520ee2ffdb691edcb82bf
kokichat <kalo.football97@gmail.com>
2016-09-28 14:28:13 +0300

    yw

---
+++ ConsoleApplication45/ConsoleApplication45/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double montlyDays = double.Parse(Console.ReadLine());
+            double money = double.Parse(Console.ReadLine());
+            double value = double.Parse(Console.ReadLine());
+            
+            double montlyMoney = money * montlyDays;
+            double yearlyMoney = (montlyMoney * 12) + (montlyMoney * 2.5);
+            double moneyTaxes = yearlyMoney - (yearlyMoney / 4);
+            double valueBGN = moneyTaxes * value;
+            double moneyDay = valueBGN / 365;
+
+            Console.WriteLine(Math.Round(moneyDay, 2));
+        }
+    }
+}
